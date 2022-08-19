4edad8df634caf8bcd7bf782ea097790fab18184
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-08-19 15:55:36 +0900

    App파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import InitPage from './pages/initPage/InitPage';
+import Home from './pages/home/Home';
+import reset from 'styled-reset';
+import './app.css';
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  a{
+    text-decoration : none;
+    color : inherit;
+  }   
+
+  button{
+    border : none;
+    cursor : pointer;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/'/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<InitPage/>}/>
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+    
   );
 }
 
