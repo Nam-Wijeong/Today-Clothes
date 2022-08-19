655ff8b46d5d7293dfd9516bbb9ed98d2338ee06
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-08-19 15:54:17 +0900

    스플래쉬추가

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import ImgSplash from '../../assets/images/clothes.jpg';
+
+const ImageFadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`
+const SplaschSec = styled.section`
+  background-color: whitesmoke;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: ${props => props.display};
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  min-width: 375px; 
+  animation: 0.7s ease-in-out ${ImageFadeOut} forwards;
+  animation-delay: 0.7s;
+`
+const SplashH1 = styled.h1`
+  font-size: 30px;
+  margin-bottom: 10px;
+`
+const SplashImg = styled.img`
+  width: 300px;
+  margin-bottom: 10px;
+`
+const SplashP = styled.p`
+  font-size: 20px;
+`
 
 export default function Splash() {
+  const [display, setDisplay] = useState('flex');
+  setTimeout(() => {
+    setDisplay('none')
+  }, 2000);
   return (
-    <div>Splash</div>
+      <SplaschSec display={display}>
+        <SplashH1>옷늘의 날씨</SplashH1>
+        <SplashImg src={ImgSplash} alt="로고 이미지" />
+        <SplashP>스플래쉬 화면입니다 :)</SplashP>
+      </SplaschSec>    
   )
 }
