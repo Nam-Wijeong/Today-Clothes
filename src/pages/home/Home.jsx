ef5e0c522b6b83dd3920205809c25880b2c215a1
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-09-03 02:02:14 +0900

    Home.jsx 수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Main from '../../components/main/Main';
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <Main/>
     </>
   )
 }
