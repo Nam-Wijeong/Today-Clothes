1999352d980f96563371e1d0b0f898a4500a5ae4
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-09-15 23:39:08 +0900

    [Add] Carousel.jsx 파일 추가

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import Slider from 'react-slick/lib/slider'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import styled from 'styled-components'
 
 export default function Carousel({imgSrc1, imgSrc2, imgSrc3}) {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
   }
   return (
-    <div className='carousel'>
+    <CarouselWrap>
       <Slider {...settings}>
         <div>
           <img src={imgSrc1} alt="" />
@@ -22,6 +25,14 @@ export default function Carousel({imgSrc1, imgSrc2, imgSrc3}) {
           <img src={imgSrc3} alt="" />
         </div>
       </Slider>
-    </div>
+    </CarouselWrap>
   )
 }
+
+const CarouselWrap = styled.div`
+  margin-bottom: 30px;  
+  img {
+    width: 100%;
+    height: 230px;
+  }
+`
