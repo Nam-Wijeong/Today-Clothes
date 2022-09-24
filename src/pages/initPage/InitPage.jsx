d2391a244b89d729313018bb6d37fe6f491439d3
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-09-24 15:25:07 +0900

    [Update] 로컬스토리지로 유저네임저장

--- src/pages/initPage/InitPage.jsx
+++ src/pages/initPage/InitPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Splash from '../../components/splash/Splash';
 import styled from 'styled-components';
@@ -6,10 +6,15 @@ import styled from 'styled-components';
 export default function InitPage() {
   const [loading, setLoading] = useState(true);
   const [userName, setUserName] = useState('');
+
   useEffect(() => {
     setLoading(false)
     console.log('로딩');
   }, [])
+
+  const saveData = () => {
+    window.localStorage.setItem("userName", userName)
+  }
 
   const handleData = (event) => {
     if(event.target.type === 'text') {
@@ -19,8 +24,6 @@ export default function InitPage() {
   
   const handleSubmit = (event) => {
     event.preventDefault();
-    // localStorage.setItem('username', userName);
-    console.log(userName);
   }
 
   return (
@@ -34,7 +37,7 @@ export default function InitPage() {
               '옷늘의 날씨'에 오신 것을<br/> 환영합니다!
             </InitH1>
             <InitInp type="text" placeholder='이름을 입력해주세요 :)' required onChange={handleData} value={userName}/>
-            <InitLink to='/home'>시작하기</InitLink>
+            <InitLink to='/home' onClick={saveData}>시작하기</InitLink>
           </InitForm>
         </>
       }
@@ -43,15 +46,22 @@ export default function InitPage() {
 }
 
 const InitForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 375px;
-  margin: 200px auto;
-  padding: 0 30px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+  max-width: 390px;
+  max-height: 844px;
+  padding: 200px 30px;
+  overflow: hidden;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 `
 const InitH1 = styled.h1`
-  font-size: 30px;
+  font-family: 'SCDream5';
+  font-size: 28px;
   line-height: 40px;
   margin-bottom: 20px;
 `
@@ -61,17 +71,20 @@ const InitInp = styled.input`
   border-bottom: 2px solid black;
   outline: none;
   background-color: transparent;
-  font-size: 20px;
+  font-size: 18px;
   margin-bottom: 20px;
   padding: 10px;
+  font-family: 'SCDream4';
 `
 const InitLink = styled(Link)`
-  width: 100%;
   cursor: pointer;
+  display: block;
+  width: 100%;
   background-color: lightgrey;
   color: #fff;
   padding: 10px;
   border-radius: 10px;
-  font-size: 18px;
+  font-family: 'SCDream4';
+  font-size: 16px;
   text-align: center;
 `
