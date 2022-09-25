9e6a27a44cb4c01794cf11cba65149a7ee16a807
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-09-25 15:00:47 +0900

    [Update]불필요한 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import InitPage from './pages/initPage/InitPage';
 import Home from './pages/home/Home';
-import Login from './pages/login/Login';
 import reset from 'styled-reset';
 import './app.css';
 
